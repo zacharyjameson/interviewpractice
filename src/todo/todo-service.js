fbd9fd011071d63a08b5c62a7e383e4aee81233b
Vincent Ramdhanie <vramdhanie@gmail.com>
2019-03-20 12:57:48 -0400

    Initial files for router challenge

---
+++ src/todo/todo-service.js
@@ -0,0 +1,46 @@
+const TodoService = {
+  getTodos(db) {
+    return db
+      .from('todo')
+      .select(
+        'todo.id',
+        'todo.title',
+        'todo.completed',
+      )
+  },
+  getTodoById(db, todo_id) {
+    return db
+      .from('todo')
+      .select(
+        'todo.id',
+        'todo.title',
+        'todo.completed',
+      )
+      .where('todo.id', todo_id)
+      .first()
+  },
+  insertTodo(db, newTodo) {
+    return db
+      .insert(newTodo)
+      .into('todo')
+      .returning('*')
+      .then(rows => {
+        return rows[0]
+      })
+  },
+  deleteTodo(db, todo_id) {
+    console.log('In the service: ', todo_id)
+    return db('todo')
+      .where({'id': todo_id})
+      .delete()
+  },
+  updateTodo(db, todo_id, newTodo) {
+    return db('todo')
+      .where({id: todo_id})
+      .update(newTodo, returning=true)
+      .returning('*')
+  }
+
+}
+
+module.exports = TodoService
